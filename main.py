e04a20442fa0a3728d58546a02e9cbfa7ed5f1f6
Shelly Katoch <shelly111katoch@gmail.com>
2017-06-26 18:41:39 +0530

    get own post of the user

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import requests
 from keys import *
 import urllib
@@ -13,6 +12,26 @@ def self_info():
   print 'My info is:\n', my_info['data']['username']
 
 self_info()
+
+def get_own_post():
+  request_url = (BASE_URL + 'users/self/media/recent?access_token=%s') % (ACCESS_TOKEN)
+  print 'Requesting media for: %s' % (request_url)
+
+  recent_post = requests.get(request_url).json()
+  if recent_post['meta']['code'] == 200:
+    if len(recent_post['data'])>0:
+      image_name= recent_post['data'][0]['id']+".jpeg"
+      image_url= recent_post['data'][0]['images']['standard_resolution']['url']
+      urllib.urlretrieve(image_url,image_name)
+    else:
+      print "There is no recent post!"
+  else:
+    print "Status code other than 200 received!"
+
+
+get_own_post()
+
+
 
 
 
