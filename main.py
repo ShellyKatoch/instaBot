4025e1826fd240fd560f476220d9df77d52f7e6f
Shelly Katoch <shelly111katoch@gmail.com>
2017-06-26 22:24:55 +0530

    post comments on user's post

--- main.py
+++ main.py
@@ -117,6 +117,21 @@ def get_post_id(insta_username):
 
  like_a_post('ishita')
 
+ def post_a_comment(insta_username):
+     media_id = get_post_id(insta_username)
+     comment_text = raw_input("your comment:")
+     request_url = (BASE_URL + 'media/%s/comments') % (media_id)
+     payload = {"access_token": ACCESS_TOKEN, "text": comment_text}
+     print 'post request url:%s' % (request_url)
+     make_comment = requests.post(request_url, payload).json()
+
+     if make_comment['meta']['code'] == 200:
+         print 'Successfully added a new comment!'
+     else:
+         print 'Unable to add comment.Try again'
+
+ post_a_comment('prashant')
+
 
 
 
