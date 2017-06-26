2bd09cdd7b57bdaf50fe45914e9798f69dda0f66
Shelly Katoch <shelly111katoch@gmail.com>
2017-06-26 19:08:56 +0530

    access post of the user

--- main.py
+++ main.py
@@ -30,6 +30,42 @@ def get_own_post():
 
 
 get_own_post()
+def get_user_id(username):
+  request_url = (BASE_URL + 'users/search?q=%access_token=%s') % (username, ACCESS_TOKEN)
+  print 'Get request url: %s' % (request_url)
+
+  user_info=requests.get(request_url).json()
+  if user_info['meta']['code'] == 200:
+    if len(user_info['data'])>0:
+      return user_info['data'][0]['id']
+    else:
+       print ""
+
+       return None
+  else:
+    print 'status code other than 200 recieved'
+exit()
+
+id_var= get_user_id('shelly katoch')
+
+
+def get_users_post(username):
+  user_id = get_user_id(username)
+  request_url = (BASE_URL + 'users/%s/media/recent?access_token=%s') % (user_id,ACCESS_TOKEN)
+  print '//Requesting media for: %s' % (request_url)
+
+  recent_post = requests.get(request_url).json()
+  if recent_post['meta']['code'] == 200:
+    if len(recent_post['data'])>0:
+      image_name = recent_post['data'][0]['id'] + ".jpeg"
+      image_url = recent_post['data'][0]['images']['standard_resolution']['url']
+      urllib.urlretrieve(image_url, image_name)
+    else:
+      print "There is no recent post!"
+  else:
+    print "Status code other than 200 received!"
+
+get_users_post('ishita')
 
 
 
